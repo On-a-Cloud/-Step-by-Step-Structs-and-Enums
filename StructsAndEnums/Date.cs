c8d61730f3ba27ff8969320629449b3051a9e6f1
Humberto Banuelos <humberto.banuelos13@outlook.com>
2018-09-04 13:14:28 -0400

    Add project files.

---
+++ StructsAndEnums/Date.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StructsAndEnums
+{
+    struct Date
+    {
+        private int year;
+        private Month month;
+        private int day;
+
+        public override string ToString()
+        {
+            string data = $"{this.month} {this.day + 1} {this.year + 1900}";
+            return data;
+        }
+
+        public Date(int ccyy, Month mm, int dd)
+        {
+            this.year = ccyy - 1990;
+            this.month = mm;
+            this.day = dd - 1;
+        }
+    }
+
+}
